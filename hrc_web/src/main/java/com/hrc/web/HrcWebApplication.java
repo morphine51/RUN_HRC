1c2285484b5dbedbef9942f0028f995b9eef08f5
lhb51 <lhb@humuson.com>
2022-05-27 17:33:34 +0900

    최초커밋

---
+++ hrc_web/src/main/java/com/hrc/web/HrcWebApplication.java
@@ -0,0 +1,13 @@
+package com.hrc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrcWebApplication.class, args);
+	}
+
+}
---
+++ hrc_web/src/test/java/com/hrc/web/HrcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrc.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrcWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
